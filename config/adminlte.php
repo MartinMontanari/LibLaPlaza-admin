3c2f89bf9a08a6cc02e9674605cd0ec225f60c0e
Martin Montanari <martinmontanari3@gmail.com>
2020-10-21 12:41:53 -0300

    finish with dashboard colours layout

--- config/adminlte.php
+++ config/adminlte.php
@@ -121,12 +121,12 @@ return [
     */
 
     'classes_body' => '',
-    'classes_brand' => '',
+    'classes_brand' => 'bg-white',
     'classes_brand_text' => '',
     'classes_content_wrapper' => '',
     'classes_content_header' => '',
     'classes_content' => '',
-    'classes_sidebar' => 'sidebar-dark-primary elevation-4',
+    'classes_sidebar' => 'sidebar-dark-danger elevation-4',
     'classes_sidebar_nav' => '',
     'classes_topnav' => 'navbar-white navbar-light',
     'classes_topnav_nav' => 'navbar-expand',
