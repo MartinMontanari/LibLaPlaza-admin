1973d77c05169a0bded34d12ce8e2d3b18898024
Martin Montanari <martinmontanari3@gmail.com>
2020-10-21 19:43:15 -0300

    setup php 7.4

--- app/Http/Controllers/Categories/StoreCategoryController.php
+++ app/Http/Controllers/Categories/StoreCategoryController.php
@@ -4,11 +4,13 @@
 namespace App\Http\Controllers\Categories;
 
 
+use App\Http\Adapters\Categories\StoreCategoryAdapter;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class StoreCategoryController extends Controller
 {
+    private StoreCategoryAdapter $adapter;
     public function __construct()
     {
     }
