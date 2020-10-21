f7df3cd5df3131caa093c0622764bd24e3f6fbe6
Martin Montanari <martinmontanari3@gmail.com>
2020-10-21 20:43:21 -0300

    finish use case and make migration

--- app/Application/Handlers/Categories/StoreCategoryHandler.php
+++ app/Application/Handlers/Categories/StoreCategoryHandler.php
@@ -5,16 +5,25 @@ namespace App\Application\Handlers\Categories;
 
 
 use App\Application\Commands\Categories\StoreCategoryCommand;
+use App\Domain\Entities\Category;
+use App\Infraestructure\Persistence\Eloquent\Repositories\CategoryRepository;
 
 class StoreCategoryHandler
 {
-    //TODO repositoooooooooooooorios
-    public function __construct()
+    private CategoryRepository $repository;
+
+    public function __construct(CategoryRepository $repository)
     {
+        $this->repository = $repository;
     }
 
     public function handle(StoreCategoryCommand $command){
 
+        $category = new Category();
+        $category->setName($command->getName());
+        $category->setDescription($command->getDescription());
+
+        $this->repository->persist($category);
     }
 
 }
