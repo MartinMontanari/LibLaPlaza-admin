e40d0d030395d4bfe64dca42ada8eb7f5f76a5c0
Martin Montanari <martinmontanari3@gmail.com>
2020-10-21 17:37:12 -0300

    finish dasbhoard layout

--- config/adminlte.php
+++ config/adminlte.php
@@ -232,86 +232,55 @@ return [
 
     'menu' => [
         [
-            'text' => 'search',
+            'text' => 'Buscar un producto',
             'search' => true,
             'topnav' => true,
         ],
+        ['header' => 'Menú de opciones'],
         [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
+            'text' => 'Dar de alta una venta',
+            'icon' => 'fas fa-cash-register',
+            'url' => '#'
         ],
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
-        ],
-        [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
+            'text'    => 'Productos',
+            'icon'    => 'fas fa-boxes',
             'submenu' => [
                 [
                     'text' => 'level_one',
                     'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
+                ]
+            ],
+        ],
+        [
+            'text'    => 'Categorías',
+            'icon'    => 'fas fa-tags',
+            'submenu' => [
                 [
                     'text' => 'level_one',
                     'url'  => '#',
-                ],
+                ]
             ],
         ],
-        ['header' => 'labels'],
         [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
+            'text'    => 'Proveedores',
+            'icon'    => 'fas fa-truck',
+            'submenu' => [
+                [
+                    'text' => 'level_one',
+                    'url'  => '#',
+                ]
+            ],
         ],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
+            'text'    => 'Reportes',
+            'icon'    => 'fas fa-folder',
+            'submenu' => [
+                [
+                    'text' => 'level_one',
+                    'url'  => '#',
+                ]
+            ],
         ],
     ],
 
