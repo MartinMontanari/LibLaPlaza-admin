ce8014e7bba65771cf26f604d08905875f8ca054
Martin Montanari <martinmontanari3@gmail.com>
2020-10-22 09:22:42 -0300

    set id like primary key and unique in categories table

--- database/migrations/2020_10_21_233931_create_categories.php
+++ database/migrations/2020_10_21_233931_create_categories.php
@@ -14,7 +14,7 @@ class CreateCategories extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary()->unique();
             $table->string('name');
             $table->string('description');
             $table->timestamps();
