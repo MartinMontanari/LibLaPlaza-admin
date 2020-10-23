200ee31b1ace8a8e10dbede65b9151c7a378df70
Martin Montanari <martinmontanari3@gmail.com>
2020-10-23 10:53:16 -0300

    start uc

--- app/Http/Controllers/Categories/EditCategoryAction.php
+++ app/Http/Controllers/Categories/EditCategoryAction.php
@@ -4,10 +4,16 @@
 namespace App\Http\Controllers\Categories;
 
 
+use Illuminate\Http\Request;
+
 class EditCategoryAction
 {
-        public function __construct()
-        {
-        }
+    public function __construct()
+    {
+    }
 
+    public function __invoke(Request $request)
+    {
+
+    }
 }
