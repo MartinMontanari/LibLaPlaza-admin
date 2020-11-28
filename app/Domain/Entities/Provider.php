0f4964dcded198f9a9fe649db7b05b8a549240c0
Martin Montanari <martinmontanari3@gmail.com>
2020-11-28 19:51:11 -0300

    added code to entity

--- app/Domain/Entities/Provider.php
+++ app/Domain/Entities/Provider.php
@@ -20,6 +20,22 @@ class Provider extends Model
     /**
      * @return string
      */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     */
+    public function setCode(string $code): void
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
