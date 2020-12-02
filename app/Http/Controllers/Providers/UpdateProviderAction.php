677b66fb58c9c317b9bedbb743dfd449055666a3
Martin Montanari <martinmontanari3@gmail.com>
2020-12-02 18:45:30 -0300

    finish Action, Adapter and Schema

--- app/Http/Controllers/Providers/UpdateProviderAction.php
+++ app/Http/Controllers/Providers/UpdateProviderAction.php
@@ -4,7 +4,43 @@
 namespace App\Http\Controllers\Providers;
 
 
+use App\Application\Handlers\Providers\UpdateProviderHandler;
+use App\Http\Adapters\Providers\UpdateProviderAdapter;
+use Illuminate\Http\Request;
+
 class UpdateProviderAction
 {
+    private UpdateProviderHandler $handler;
+    private UpdateProviderAdapter $adapter;
 
+    /**
+     * UpdateProviderAction constructor.
+     * @param UpdateProviderHandler $providerHandler
+     * @param UpdateProviderAdapter $providerAdapter
+     */
+    public function __construct
+    (
+        UpdateProviderHandler $providerHandler,
+        UpdateProviderAdapter $providerAdapter
+    )
+    {
+        $this->handler = $providerHandler;
+        $this->adapter = $providerAdapter;
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index(Request $request)
+    {
+        $provider = $this->handler->index($request->query('id'));
+        return view('admin.providers.edit',['provider' => $provider]);
+    }
+
+    public function __invoke(Request $request)
+    {
+        $command = $this->adapter->adapt($request);
+        $this->handler->handle($command);
+    }
 }
