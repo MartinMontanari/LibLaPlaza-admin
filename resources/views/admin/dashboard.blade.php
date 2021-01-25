70372f467c06f6a417210076c137cdb203b5b90e
Martin Montanari <martinmontanari3@gmail.com>
2021-01-25 20:20:31 -0300

    feature: finish with UC and fixing card styles

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,30 +6,33 @@
 @section('content_header')
     @if(!isset($queryResult))
         <h1>Dashboard</h1>
+    @else
+        <h1>Resultados de su búsqueda</h1>
     @endif
 @stop
 
 @section('content')
     @if(!isset($queryResult))
-        <h1>Panel de administración</h1>
+        <h2>Panel de administración</h2>
     @endif
     @if(isset($queryResult))
-        <div class="container">
-            <div class="row justify-content-md-center">
-                <div class="card col-6 block">
-                    <div class="card-body">
-                        <div class="form-group-sm">
-                            @foreach($queryResult as $product)
-                                <p>{{$product->getName()}}</p>
-
-                            @endforeach
-                        </div>
-                    </div>
+        @foreach($queryResult as $product)
+            <div class="d-lg-inline-flex">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title col-12">{{$product->getName()}}</h5>
+                    <h6 class="card-subtitle col-6 mb-2 text-muted">{{$product->getCode()}}</h6>
+                    <p class="card-text">{{$product->getDescription()}}</p>
+                    <form action="{{route('stock-report',['product_id' => $product->getId()])}}" method="POST">
+                        @csrf
+                        <input type="submit" class="btn btn-outline-info btn-sm d-inline-block" role="button"
+                               value="Detalles">
+                    </form>
                 </div>
             </div>
-        </div>
+            </div>
+        @endforeach
     @endif
-
     <div class="row justify-content-md-center">
         @if($errors->any())
             <div class="card col-6 alert alert-warning">
