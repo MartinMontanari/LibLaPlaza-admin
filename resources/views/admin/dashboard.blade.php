1542ac232f282fe92ffc50d837902d28fa54f2e2
Martin Montanari <martinmontanari3@gmail.com>
2021-01-25 20:59:48 -0300

    finish view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,22 +16,24 @@
         <h2>Panel de administración</h2>
     @endif
     @if(isset($queryResult))
-        @foreach($queryResult as $product)
-            <div class="d-lg-inline-flex">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title col-12">{{$product->getName()}}</h5>
-                    <h6 class="card-subtitle col-6 mb-2 text-muted">{{$product->getCode()}}</h6>
-                    <p class="card-text">{{$product->getDescription()}}</p>
-                    <form action="{{route('stock-report',['product_id' => $product->getId()])}}" method="POST">
-                        @csrf
-                        <input type="submit" class="btn btn-outline-info btn-sm d-inline-block" role="button"
-                               value="Detalles">
-                    </form>
+        <div class="row">
+            @foreach($queryResult as $product)
+                <div class="col-md-4 col-xs-12">
+                    <div class="card" style="width: 100%;">
+                        <div class="card-body">
+                            <h5 class="card-title col-12">{{$product->getName()}}</h5>
+                            <h6 class="card-subtitle col-6 mb-2 text-muted">{{$product->getCode()}}</h6>
+                            <p class="card-text">{{$product->getDescription()}}</p>
+                            <form action="{{route('stock-report',['product_id' => $product->getId()])}}" method="POST">
+                                @csrf
+                                <input type="submit" class="btn btn-outline-info btn-sm d-inline-block" role="button"
+                                       value="Detalles">
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     @endif
     <div class="row justify-content-md-center">
         @if($errors->any())
