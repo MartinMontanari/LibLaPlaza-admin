840af1225e223177569b43573dd18e2329c9b1e2
Martin Montanari <martinmontanari3@gmail.com>
2021-02-05 23:01:43 -0300

    Until this commit I have been doing it without vue

--- resources/views/admin/sales/new.blade.php
+++ resources/views/admin/sales/new.blade.php
@@ -8,6 +8,7 @@
 @stop
 
 @section('content')
+    {{--        <App></App>--}}
     <div class="container">
         <div class="row justify-content-md-center">
             <div class="card col-md-12 col-sm-12 block">
@@ -16,7 +17,7 @@
                 </div>
                 <div class="card-body">
                     <div class="form-group-sm">
-                        <form id="form" action="{{route('store-sale')}}" method="POST">
+                        <form id="form" action="{{route('store-sale')}}" method="POST" onsubmit="preventDefault()">
                             @csrf
                             <div class="form-group-lg">
                                 <div class="input-group">
@@ -38,7 +39,7 @@
                                         <label>Dirección del cliente </label>
                                         <input type="text" class="form-control" name="dni" min="7" max="30"
                                                maxlength="30"
-                                               placeholder="Direción"
+                                               placeholder="Dirección"
                                                value="{{old('address')}}" required><br>
                                     </div>
                                 </div>
@@ -73,7 +74,7 @@
                                     <div class="col-md-4">
                                         <label>Artículo</label>
                                         <select class="form-control select2-blue" name="billType"
-                                                id="articleSelect" required>
+                                                id="articleSelect">
                                             <option value="">Seleccione un artículo...</option>
                                             @foreach($productStock as $stock)
                                                 <option
@@ -83,27 +84,28 @@
                                             @endforeach
                                         </select>
                                     </div>
-                                    <div class="col-md-1">
+                                    <div class="col-md-2">
                                         <label>Stock</label>
-                                        <input type="number" class="form-control" min="0"
+                                        <input type="number" class="form-control text-center" min="0"
                                                value="" id="pStock"
                                                disabled>
                                     </div>
-                                    <div class="col-md-1">
+                                    <div class="col-md-2">
                                         <label>Cantidad</label>
-                                        <input type="number" class="form-control" min="1"
+                                        <input type="number" class="form-control text-center" min="1"
                                                name="quantity" id="pQuantity"
-                                               placeholder="" required/>
+                                               placeholder=""/>
                                     </div>
                                     <div class="col-md-2 d-flex justify-content-center align-items-center">
-                                        {{--                                        TODO ver como carajos hacer andar éste botón--}}
-                                        <button id="btnAddProduct" class="btn btn-outline-info"
-                                                value="#">Agregar
+                                        {{--                                                                                TODO ver como carajos hacer andar éste botón--}}
+                                        <button id="btnAddProduct" class="btn btn-outline-info">Agregar
                                         </button>
                                     </div>
                                 </div>
                                 <hr>
-                                {{--  //TODO sacar el contenido hardcodeado y nutrir con un endpoint--}}
+                                <div class="col-md-12 text-center">
+                                    <h5>Detalles de la compra</h5>
+                                </div>
                                 <div class="container table-sm overflow-auto">
                                     <table class="table table-bordered table-striped table-hover" id="saleDetail">
                                         <thead class="thead-dark text-center">
@@ -123,14 +125,13 @@
                             </div>
                             <hr>
                             <div class="col-md-12 input-group justify-content-end">
-                                {{--                                    TODO clacular el total y toda la bola--}}
                                 <div>
                                     <h4 style="margin: 4px 10px;">Total:</h4>
                                 </div>
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">$</span>
                                 </div>
-                                <input class="form-control col-md-1 col-sm-1" id="tAmount" disabled value="0">
+                                <input class="form-control col-md-2 col-sm-2" id="tAmount" disabled value="0">
                                 <input type="hidden" name="totalAmount" id="totalAmount">
                             </div>
                             <hr>
@@ -141,82 +142,110 @@
                 </div>
             </div>
         </div>
-        <div class="row justify-content-md-center">
-            @if($errors->any())
-                <div class="card col-md-6 alert alert-danger">
-                    <div class="row justify-content-center text-wrap" data-dismiss="alert">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
+    </div>
+    <div class="row justify-content-md-center">
+        @if($errors->any())
+            <div class="card col-md-6 alert alert-danger">
+                <div class="row justify-content-center text-wrap" data-dismiss="alert">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
-            @endif
-            @if(session('status'))
-                <div class="card col-md-6 alert alert-success">
-                    <div class="row justify-content-center" data-dismiss="alert">
-                        Producto actualizado correctamente.
-                    </div>
+            </div>
+        @endif
+        @if(session('status'))
+            <div class="card col-md-6 alert alert-success">
+                <div class="row justify-content-center" data-dismiss="alert">
+                    Producto actualizado correctamente.
                 </div>
-            @endif
-        </div>
-        @stop
+            </div>
+        @endif
+    </div>
+@stop
+{{--@section('js')--}}
+{{--    <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>--}}
+{{--    <script type="text/javascript" src="{{asset('/assets/spa/js/svgxuse.min.js')}}"></script>--}}
+{{--    <script type="text/javascript" src="{{asset('/assets/spa/js/coreui-utils.js')}}"></script>--}}
+{{--    <script type="text/javascript" src="{{asset('/assets/spa/js/main.js')}}"></script>--}}
+{{--@stop--}}
 
-        @section('js')
-            <script>
-                $(document).ready(function () {
-                    $('#btnAddProduct').click(function () {
-                        add();
-                    });
-                });
+@section('js')
+    <script>
+        $(document).ready(function () {
+            $('#btnAddProduct').click(function () {
+                add();
+            });
+        });
 
-                let cont = 0;
-                let totalAmount = 0;
-                if (parseInt($('#tAmount').val()) === 0) {
-                    $('#submitSale').hide();
+        let cont = 0;
+
+        // $('#submitSale').hide();
+        //
+        // if(parseFloat($('#tAmount').val()) !== 0){
+        //     document.getElementById('submitSale').show;
+        // }
+
+        $('#articleSelect').change(showStock);
+
+        function showStock() {
+            let productStock = document.getElementById('articleSelect').value.split('_');
+            $('#pStock').val(productStock[1]);
+        }
+
+        function add() {
+            let productData = document.getElementById('articleSelect').value.split('_');
+            let productId = productData[0];
+            let stock = productData[1];
+            let code = productData[2];
+            let price = productData[3];
+            let quantity = $('#pQuantity').val();
+            let sTotal = quantity * price;
+
+            let name = $('#articleSelect option:selected').text();
+
+            //TODO terminar ésta otra bosta
+            if (productId !== "" && quantity !== "" && quantity > 0) {
+                if (parseInt(stock) >= parseInt(quantity)) {
+                    let row = '<tr id="row' + cont + '">' +
+                        '<td class="align-middle"><button type="button" class="btn btn-danger btn-sm" onclick="eliminar(' + cont + ');"> <i class="fas fa-trash"></i></button></td> ' +
+                        '<input type="hidden" name="productId" value="' + productId + '"/>' +
+                        '<td>' + code + '</td> ' +
+                        '<td class="text-left">' + name + '</td> ' +
+                        '<td>' + price / 100 + '</td> ' +
+                        '<td> ' + quantity + ' </td>' +
+                        '<td> ' + sTotal / 100 + ' </td>' +
+                        '</tr>';
+                    cont++;
+
+                    eliminar(cont);
+                    clearInputs();
+                    $('#saleDetail').append(row);
+                    let tAmount = $('#tAmount').val();
+                    let total = (Number(tAmount) * 100) + sTotal;
+                    setTotal(total);
+                } else {
+                    alert('La cantidad ingresada es mayor a la cantidad en stock.');
                 }
-                $('#articleSelect').change(showStock);
+            }
+        }
 
-                function showStock() {
-                    let productStock = document.getElementById('articleSelect').value.split('_');
-                    $('#pStock').val(productStock[1]);
-                }
+        async function eliminar(cont) {
+            //TODO falta actualizar el valor del total al eliminar un producto de la lista
+            await $('#row' + cont + '').remove();
+        }
 
-                function add() {
-                    let productData = document.getElementById('articleSelect').value.split('_');
-                    console.log(productData);
-                    let productId = productData[0];
-                    let stock = productData[1];
-                    let code = productData[2];
-                    let price = productData[3];
-                    let quantity = $('#pQuantity').val();
-                    let sTotal = quantity * price;
-                    let tAmount = document.getElementById("tAmount");
-                    let total = tAmount.value + sTotal;
-                    let name = $('#articleSelect option:selected').text();
+        // evaluar();
+        function setTotal(total) {
+            let totalAmount = Number(total) / 100;
+            $('#tAmount').val(totalAmount);
+        }
 
-                    //TODO terminar ésta otra bosta
-                    if (productId !== "" && quantity !== "" && quantity > 0) {
-                        if (parseInt(stock) >= parseInt(quantity)) {
-                            let row = '<tr>' +
-                                '<td><button type="button" class="btn btn-danger btn-sm d-inline-block" onclick="eliminar(' + cont + ');"> <i class="fas fa-trash"></i></button></td> ' +
-                                '<input type="hidden" name="productId" value="' + productId + '"/>' +
-                                '<td>' + code + '</td> ' +
-                                '<td class="text-left">' + name + '</td> ' +
-                                '<td>' + price / 100 + '</td> ' +
-                                '<td> ' + quantity + ' </td>' +
-                                '<td> ' + total / 100 + ' </td>' +
-                                '</tr>';
-                            cont++;
-                            // clean();TODO ver de donde concha sale ésto en los otros videos
-                            $("#tAmount").html(total);
-                            // evaluar();
-                            $('#saleDetail').append(row);
-                        } else {
-                            alert('La cantidad ingresada es mayor a la cantidad en stock.');
-                        }
-                    }
-                }
-            </script>
+        function clearInputs() {
+            $("#pQuantity").val("");
+            $("#pStock").val("");
+            $("#articleSelect").val("");
+        }
+    </script>
 @stop
