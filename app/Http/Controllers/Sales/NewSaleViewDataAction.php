a93a711d7c8bc7d361e3b6ccd9ea35925afe2607
Martin Montanari <martinmontanari3@gmail.com>
2021-02-05 23:22:05 -0300

    wip - made json data response, component and nurture it

--- app/Http/Controllers/Sales/NewSaleViewDataAction.php
+++ app/Http/Controllers/Sales/NewSaleViewDataAction.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Sales;
 
 use App\Application\Handlers\Sales\NewSaleViewDataHandler;
 use App\Exceptions\ResultNotFoundException;
+use Illuminate\Http\JsonResponse;
 
 class NewSaleViewDataAction
 {
@@ -23,18 +24,16 @@ class NewSaleViewDataAction
     {
         try {
             $result = $this->newSaleViewDataHandler->handle();
-            return view('admin.sales.new',  ['productStock' => $result]);
-
-//            $data = [];
-//            foreach ($result as $item) {
-//                $data[] = [
-//                    'id' => $item->product->getId(),
-//                    'name' => $item->product->getName(),
-//                    'stock' => $item->getQuantity(),
-//                    'price' => $item->product->getPrice(),
-//                ];
-//            }
-//            return new JsonResponse(['data' => $data]);
+            $data = [];
+            foreach ($result as $item) {
+                $data[] = [
+                    'id' => $item->product->getId(),
+                    'name' => $item->product->getName(),
+                    'stock' => $item->getQuantity(),
+                    'price' => $item->product->getPrice(),
+                ];
+            }
+            return new JsonResponse(['data' => $data]);
         } catch (ResultNotFoundException $errors) {
             return redirect()->route('new-sale')->withErrors($errors->getMessages())->withInput();
         }
